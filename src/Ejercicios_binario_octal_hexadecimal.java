08445960e0ba0cfaf6f7b76ebaba1b99f307f51e
AgustinPastor <agus.pastman@gmail.com>
2025-01-16 07:27:56 +0100

    Actualización

---
+++ src/Ejercicios_binario_octal_hexadecimal.java
@@ -0,0 +1,42 @@
+public class Ejercicios_binario_octal_hexadecimal {
+    //Ejercicio binario, octal, hexadecimal
+            /*
+    Introducir un entero no negativo, y convertirlo a binario, octal y hexadecimal. (BinarioOctalHexadecimal)
+
+    public static void main(String[] args) {
+        System.out.println("Escribe el numero entero positivo:");
+        Scanner scanner=new Scanner(System.in);
+        int numero= scanner.nextInt();
+
+        if (numero<0){
+        System.out.println("El numero debe ser positivo:");
+        return;
+        }
+
+        String binario = Integer.toBinaryString(numero);
+        String octal = Integer.toOctalString(numero);
+        String hexa = Integer.toHexString(numero).toUpperCase();
+
+        System.out.println("Número original: " + numero);
+        System.out.println("Binario: " + binario);
+        System.out.println("Octal: " + octal);
+        System.out.println("Hexadecimal: " + hexa);
+
+
+    }
+
+     */
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
